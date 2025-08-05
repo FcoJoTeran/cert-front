147ad7d80061832518e6e6d75e02e2ddee43a1b9
Francisco Jose Teran Cobo <fteran@BANCOGUAYAQUIL.COM>
2025-08-05 09:38:48 -0500

    Inserción de IDL

--- anchor/src/certfront-exports.ts
+++ anchor/src/certfront-exports.ts
@@ -1,12 +1,13 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
 import { AnchorProvider, Program, Idl } from "@coral-xyz/anchor";
-import * as anchor from "@coral-xyz/anchor";
+//import * as anchor from "@coral-xyz/anchor";
 import { Cluster, PublicKey } from "@solana/web3.js";
 import * as CertificatesIDL from "anchor/target/idl/certificates.json";
-import type { Certificates } from "../target/types/certificates";
+//import type { Certificates } from "../target/types/certificates";
 
 // Re-export the generated IDL and type
-export { Certificates, CertificatesIDL };
+//export { Certificates, CertificatesIDL };
+export {CertificatesIDL};
 
 // After updating your program ID (e.g. after running `anchor keys sync`) update the value below.
 export const CERTIFICATES_PROGRAM_ID = new PublicKey(
